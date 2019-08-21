25e5fa43fa5bd83bf1cf75ce317ec98d71e02cd7
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2019-08-21 13:48:43 +0200

    new deployment: $now

--- _site/haunt-master.sh
+++ _site/haunt-master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 now="$(date)"
-
+echo "[commit message:] new deployment: $now"
 git add _site/.
 git commit -m 'new deployment: $now'
+git show --name-only
 git push -f origin dev:master
